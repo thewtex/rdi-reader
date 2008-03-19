f9176965fef554ec71dbae521f1c22f55265d49a
thewtex <matt@mmmccormick.com>
2008-03-19 15:53:47 -0500

    vtk_test is failing

--- tests/vtk_test.cpp
+++ tests/vtk_test.cpp
@@ -1,3 +1,7 @@
+#include <exception>
+#include <iostream>
+
+
 #include "vtk/ViewImage.h"
 
 using namespace visual_sonics::vtk;
@@ -5,12 +9,18 @@ using namespace visual_sonics::vtk;
 
 int main()
 {
-
-  ViewImage* vi = new ViewImage("/mnt/dataa/visualsonics/test_base/visual_sonics", "2007-06-08-15-57-05-328");
-
-  vi->view_b_mode();
-
-  delete vi;
+  try
+  {
+    ViewImage* vi = new ViewImage("/mnt/research/Research/in_vivo/test_base/visual_sonics", "2007-06-08-15-57-05-328");
+  
+    vi->view_b_mode();
+  
+    delete vi;
+  }
+  catch ( std::exception& e )
+  {
+    std::cerr << "There was a std::exception: "  << e.what() << std::endl;
+  }
 
   return 0;
 }

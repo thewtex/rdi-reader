699fd759b1e6ddca83c19cbfeb06adeab82bb92a
Matt McCormick (thewtex) <matt@mmmccormick.com>
2008-09-18 11:07:34 -0500

    go back to working on wooten

--- tests/vtk_test.cpp
+++ tests/vtk_test.cpp
@@ -11,8 +11,8 @@ int main()
 {
   try
   {
-    //ImageViewer* vi = new ImageViewer("/mnt/research/Research/in_vivo/test_base/visual_sonics/2007-06-08-15-57-05-328");
-    ImageViewer* vi = new ImageViewer("/mnt/dataa/visualsonics/test_base/visual_sonics/2007-06-08-15-57-05-328");
+    ImageViewer* vi = new ImageViewer("/mnt/research/Research/in_vivo/test_base/visual_sonics/2007-06-08-15-57-05-328");
+    //ImageViewer* vi = new ImageViewer("/mnt/dataa/visualsonics/test_base/visual_sonics/2007-06-08-15-57-05-328");
 
     vi->view_b_mode();
     vi->view_saturation();

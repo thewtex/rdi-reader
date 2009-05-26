03de967a925e04591bee8f57e01a5562571f95b4
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-05-26 15:06:09 -0500

    format_evaluator.py: Doxy header, open file

--- source/common/formats/format_evaluator.py
+++ source/common/formats/format_evaluator.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3.0
+#
+##
+# @file fe.py
+# @brief Determine the file format given an example .rdi file.
+# @author Matthew McCormick (thewtex)
+# @version
+# @date 2009-05-21
 
-print("hello world")
+# Public Domain
+
+#with open('../../../test_data/machine/visualsonics/vevo770/rmv710b/pat143_seg3.rdi', 'r' ) as file:
+with open('test_data/machine/visualsonics/vevo770/rmv710b/pat143_seg3.rdi', 'r' ) as file:
+
+    #a = 5
+
+    print("hello world")
+
+def myfun():
+    print("hey you")
+
+myfun()
+
+if __name__ == "__main__":
+    import sys
+    print(sys.argv[1])

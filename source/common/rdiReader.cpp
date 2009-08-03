f75efc7866ffeb3971d588a143a668ddf5872335
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-08-03 15:23:48 -0500

    create the XercesC DOMDocument

--- source/common/rdiReader.cpp
+++ source/common/rdiReader.cpp
@@ -6,7 +6,12 @@
 #include <stdexcept>
 using namespace std;
 
+#include <xercesc/dom/DOM.hpp>
 #include <xercesc/util/PlatformUtils.hpp>
+
+#include <xsd/cxx/xml/dom/auto-ptr.hxx>
+
+#include "common/formats/rdi.hxx"
 
 rdiReader::rdiReader(const char* filepath):
   m_filepath(filepath)
@@ -17,12 +22,19 @@ void rdiReader::parse()
 {
   xercesc::XMLPlatformUtils::Initialize();
 
+  ::xml_schema::dom::auto_ptr< ::xercesc::DOMDocument> domdoc;
+
   std::ifstream infile(m_filepath.c_str());
   infile.exceptions(ifstream::eofbit|ifstream::failbit|ifstream::badbit);
 
   std::string line;
   getline(infile, line);
   cout <<  (line == "=== IMAGE INFO ===") << endl;
+
+  ::std::auto_ptr< ::image_info_t> rdi (
+    image_info(domdoc,
+      xml_schema::flags::keep_dom | xml_schema::flags::own_dom)
+  );
 
   xercesc::XMLPlatformUtils::Terminate();
 }

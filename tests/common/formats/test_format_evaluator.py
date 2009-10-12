7e6bc06ff30b466ca09421641bc300ae70d9c07a
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-10-11 19:41:19 -0500

    for test_format_evaluator.py

--- tests/common/formats/test_format_evaluator.py
+++ tests/common/formats/test_format_evaluator.py
@@ -4,11 +4,20 @@ import unittest
 import os
 import sys
 
-from format_evaluator import *
+from format_evaluator.format_evaluator import FormatEvaluator
 
 # get path to source dir
 fe_path = os.path.dirname(sys.modules['format_evaluator'].__file__)
-source_path = os.path.join(fe_path, '..', '..', '..')
+script_path = os.path.join(fe_path, '..')
+source_path = os.path.join(fe_path, '..', '..', '..', '..')
+
+# copy for now since we squashed a clean way of doing it by putting a - in the
+# filename
+def main(rdi_filepath):
+    with open(rdi_filepath, 'r', encoding='latin_1') as rdi_file:
+        fe = FormatEvaluator(rdi_file, script_path)
+        fe.run()
+
 
 class TestFormatEvaluator(unittest.TestCase):
 

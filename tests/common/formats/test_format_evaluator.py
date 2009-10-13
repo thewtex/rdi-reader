2c1f2bba548d53df0fc5c33d05c9ba7c37ec4719
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-10-13 15:34:57 -0500

    mv data_for_tests -> tests/data

--- tests/common/formats/test_format_evaluator.py
+++ tests/common/formats/test_format_evaluator.py
@@ -23,5 +23,5 @@ class TestFormatEvaluator(unittest.TestCase):
 
     def testmain(self):
         self.assertRaises(IOError, main, 'a_nonexistent_filepath')
-        self.assertEqual(main(os.path.join(source_path, 'data_for_tests', 'machine', 'visualsonics', 'vevo770', 'rmv710b', 'pat143_seg3.rdi')),\
+        self.assertEqual(main(os.path.join(source_path, 'tests', 'data', 'machine', 'visualsonics', 'vevo770', 'rmv710b', 'pat143_seg3.rdi')),\
                 None )

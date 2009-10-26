c74f09b40f0f23892c5f6570a4c3a1796af2d0fd
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-10-26 17:23:28 -0500

    add test for the itk ImageIO

---
+++ tests/itk/vs-convert-b-mode.cxx
@@ -0,0 +1,66 @@
+/**
+ * @file vs-convert-b-mode.cxx
+ * @brief Test VisualSonics conversion by making a B-Mode.
+ * @author Matthew McCormick (thewtex) <matt@mmmccormick.com>
+ * @date 2009-10-26
+ */
+
+#include <iostream>
+using namespace std;
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkBModeImageFilter.h"
+
+#include "itkVisualSonicsImageIO.h"
+#include "itkVisualSonicsImageIOFactory.h"
+
+int main( int argc, char* argv[] )
+{
+  // usage: vs-convert-b-mode input.rdi output.mhd
+  if( argc != 3 )
+    return EXIT_FAILURE;
+
+  itk::VisualSonicsImageIOFactory::RegisterOneFactory();
+
+  typedef signed short InputPixelType;
+  typedef unsigned short InputPixelType;
+  typedef itk::Image< InputPixelType, 3 > InputImageType;
+  typedef itk::Image< OutputPixelType, 3 > InputImageType;
+
+  typedef itk::ImageFileReader< InputImageType > ReaderType;
+  typedef itk::BModeFilterType< InputImageType, OutputImageType > BModeType;
+  typedef itk::ImageFileWriter< OutputImageType > WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  BModeType::Pointer  bmode  = BModeType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( argv[1] );
+  writer->SetFileName( argv[2] );
+
+  bmode->SetInput( reader->GetOutput() );
+  writer->SetInput( bmode->GetOutput() );
+  try
+    {
+    writer->Update();
+    }
+  catch ( itk::ExceptionObject& e )
+    {
+    cerr << "Error: " << e << endl;
+    return EXIT_FAILURE;
+    }
+  catch (ifstream::failure& e)
+    {
+    cerr << "Error: failure opening/reading file." << endl;
+    return EXIT_FAILURE;
+    }
+  catch (std::exception& e)
+    {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return EXIT_FAILURE;
+    }
+  return EXIT_SUCCESS;
+}

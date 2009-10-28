b026157e15341c2f9b63db1ab273fca57e80c3a1
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-10-28 15:09:42 -0500

    do not specify b-mode quantile
    the default value was changed to 1.0 as needed in the b-mode upstream

--- tests/itk/vs-convert-b-mode.cxx
+++ tests/itk/vs-convert-b-mode.cxx
@@ -38,8 +38,6 @@ int main( int argc, char* argv[] )
   BModeType::Pointer  bmode  = BModeType::New();
   WriterType::Pointer writer = WriterType::New();
 
-  bmode->SetQuantile( 0.99999999999999 );
-
   reader->SetFileName( argv[1] );
   writer->SetFileName( argv[2] );
 

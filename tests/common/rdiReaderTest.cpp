bcd9152aeed4ee458f1f17122881acb9e1884efc
Matt McCormick (thewtex) <matt@mmmccormick.com>
2009-12-15 14:24:38 -0600

    change test from a plaque to phantom and improve method

--- tests/common/rdiReaderTest.cpp
+++ tests/common/rdiReaderTest.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-static const string filepath = "@RDI_TESTING_FILEPATH@";
+string filepath;
 
 class rdiReaderTest: public ::testing::Test
 {
@@ -23,10 +23,10 @@ protected:
 TEST_F(rdiReaderTest, ImageInfoSection)
 {
   auto_ptr<rdi_t> rdi_i = rdi_reader.parse_to_rdi_t();
-  EXPECT_EQ(string("plaque_pat_143_right"), rdi_i->image_info().Study_Name()) << "Invalid Study Name";
-  EXPECT_EQ(246, rdi_i->image_info().Image_Frames()) << "Invalid Image Frames";
-  EXPECT_EQ(250, rdi_i->image_info().Image_Lines()) << "Invalid Image Lines";
-  EXPECT_EQ(4256, rdi_i->image_info().Image_Acquisition_Size()) << "Invalid Image Acquisition Size";
+  EXPECT_EQ(string("pat147"), rdi_i->image_info().Study_Name()) << "Invalid Study Name";
+  EXPECT_EQ(29, rdi_i->image_info().Image_Frames()) << "Invalid Image Frames";
+  EXPECT_EQ(50, rdi_i->image_info().Image_Lines()) << "Invalid Image Lines";
+  EXPECT_EQ(4480, rdi_i->image_info().Image_Acquisition_Size()) << "Invalid Image Acquisition Size";
 }
 
 TEST_F(rdiReaderTest, ImageParametersSection)
@@ -37,4 +37,12 @@ TEST_F(rdiReaderTest, ImageParametersSection)
   EXPECT_FLOAT_EQ(154.0, rdi_i->image_parameters().RF_Mode().ActiveProbe().Sample_Time()) << "Invalid Sample Time";
   EXPECT_EQ(string("mm"), rdi_i->image_parameters().RF_Mode().ActiveProbe().Encoder_Range_Max().units()) << "Invalid Encoder Range Max units";
   EXPECT_FLOAT_EQ(13.75, rdi_i->image_parameters().RF_Mode().ActiveProbe().Encoder_Range_Max()) << "Invalid Encoder Range Max";
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    filepath = argv[1];
+
+    return RUN_ALL_TESTS();
 }

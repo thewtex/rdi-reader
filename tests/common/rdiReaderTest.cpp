d512570ddd49a00d7749458081269ee0b9dd8121
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-02-23 21:36:29 -0600

    Use new testing input.

--- tests/common/rdiReaderTest.cpp
+++ tests/common/rdiReaderTest.cpp
@@ -23,10 +23,10 @@ protected:
 TEST_F(rdiReaderTest, ImageInfoSection)
 {
   auto_ptr<rdi_t> rdi_i = rdi_reader.parse_to_rdi_t();
-  EXPECT_EQ(string("pat147"), rdi_i->image_info().Study_Name()) << "Invalid Study Name";
-  EXPECT_EQ(29, rdi_i->image_info().Image_Frames()) << "Invalid Image Frames";
-  EXPECT_EQ(50, rdi_i->image_info().Image_Lines()) << "Invalid Image Lines";
-  EXPECT_EQ(4480, rdi_i->image_info().Image_Acquisition_Size()) << "Invalid Image Acquisition Size";
+  EXPECT_EQ(string("QuickStudy 201002012255"), rdi_i->image_info().Study_Name()) << "Invalid Study Name";
+  EXPECT_EQ(93, rdi_i->image_info().Image_Frames()) << "Invalid Image Frames";
+  EXPECT_EQ(10, rdi_i->image_info().Image_Lines()) << "Invalid Image Lines";
+  EXPECT_EQ(4256, rdi_i->image_info().Image_Acquisition_Size()) << "Invalid Image Acquisition Size";
 }
 
 TEST_F(rdiReaderTest, ImageParametersSection)
@@ -36,7 +36,7 @@ TEST_F(rdiReaderTest, ImageParametersSection)
   EXPECT_EQ(string("µs"), rdi_i->image_parameters().RF_Mode().ActiveProbe().Sample_Time().units()) << "Invalid Sample Time units";
   EXPECT_FLOAT_EQ(154.0, rdi_i->image_parameters().RF_Mode().ActiveProbe().Sample_Time()) << "Invalid Sample Time";
   EXPECT_EQ(string("mm"), rdi_i->image_parameters().RF_Mode().ActiveProbe().Encoder_Range_Max().units()) << "Invalid Encoder Range Max units";
-  EXPECT_FLOAT_EQ(13.75, rdi_i->image_parameters().RF_Mode().ActiveProbe().Encoder_Range_Max()) << "Invalid Encoder Range Max";
+  EXPECT_FLOAT_EQ(19., rdi_i->image_parameters().RF_Mode().ActiveProbe().Encoder_Range_Max()) << "Invalid Encoder Range Max";
 }
 
 int main(int argc, char **argv) {
